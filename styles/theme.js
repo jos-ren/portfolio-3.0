e048ecc8f7f5382192a36d5cdcca73d211f73e61
Josh Renema <60946895+jos-ren@users.noreply.github.com>
2022-07-25 20:27:59 -0700

    adding basic layout

---
+++ styles/theme.js
@@ -0,0 +1,16 @@
+export const Theme = {
+    colors: {
+        navyBlue: "#0A192F",
+        blue: "#49A6AC",
+        darkBlue: "#3B7D84",
+        green: "#51BD89",
+        red: "#ED2046",
+        orange: "#F05533",
+        black: "#212121",
+        white: "#F7F7F7",
+    },
+    fontColors: {
+        black: "#212121",
+        white: "#F7F7F7",
+    }
+};
