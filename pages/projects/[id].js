8e15836d913e065d48de2ad062a6dfe694774a60
Josh Renema <60946895+jos-ren@users.noreply.github.com>
2023-02-15 12:37:06 -0800

    test dev server

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useRouter } from 'next/router'
 import useColorTheme from "use-color-theme";
@@ -12,6 +12,8 @@ import Header from '../../comps/Header';
 import { projects_data } from '../../public/data.js';
 
 export default function Projects() {
+
+    const [title, setTitle] = useState("")
 
     const colorTheme = useColorTheme("light-theme", {
         classNames: ["light-theme", "dark-theme"],
@@ -30,11 +32,13 @@ export default function Projects() {
 
     const router = useRouter()
     console.log(router.query.id)
-    const id = router.query.id
-    // console.log(projects_data[id].title)
-    // if(projects_data[id].title = undefined){
-    //     return null
-    // }
+
+    useEffect(() => {
+        if (router.query.id) {
+            setTitle(projects_data[router.query.id].title)
+            // maybe have a loader and when this is done to switch comps to the normal ones
+        }
+    });
 
     return (
         <>
@@ -47,7 +51,8 @@ export default function Projects() {
             <Header onClick={() => { colorTheme.toggle() }} img_src={sun_moon} logo_src={logo} />
 
             <p>path: {router.asPath}</p>
-            <div>{projects_data[id].title}</div>
+            <div>{title}</div>
+            <br/>
 
 
             <Footer />
