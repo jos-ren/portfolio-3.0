3e3132a5e4809f73659fb3f2b8cd5752951b9564
Josh Renema <60946895+jos-ren@users.noreply.github.com>
2023-02-24 11:04:41 -0800

    expanded project pages

--- comps/SectionBlock.js
+++ comps/SectionBlock.js
@@ -4,12 +4,16 @@ import styled from "styled-components";
 const Container = styled.div`
     border:1px solid red;
     width:100%;
+    // min-height:300px;
 `;
 
-const SectionBlock = ({ title }) => {
+const SectionBlock = ({ title, points }) => {
+    console.log(points)
     return (
         <Container>
             <h1>{title}</h1>
+            <p>Summary</p>
+            <div>{points}</div>
         </Container>
     );
 }
